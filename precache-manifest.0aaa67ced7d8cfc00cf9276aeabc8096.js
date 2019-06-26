824236bc4ef73116882d45b873ed37e3c6705fd8
Santiago Quintana <san3091@gmail.com>
2019-06-26 13:20:20 -0400

    Updates

---
+++ precache-manifest.0aaa67ced7d8cfc00cf9276aeabc8096.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fda8989cf2e69c1c47ce",
+    "url": "/static/css/main.78a66ace.chunk.css"
+  },
+  {
+    "revision": "fda8989cf2e69c1c47ce",
+    "url": "/static/js/main.fda8989c.chunk.js"
+  },
+  {
+    "revision": "42d206c2b87b8417d970",
+    "url": "/static/js/1.42d206c2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "17925d0ddd8944a57a241c16c89bce20",
+    "url": "/static/media/concentricThumbnail.17925d0d.jpg"
+  },
+  {
+    "revision": "93352f4b72b37759a4bc365eeb6a52ce",
+    "url": "/index.html"
+  }
+];
